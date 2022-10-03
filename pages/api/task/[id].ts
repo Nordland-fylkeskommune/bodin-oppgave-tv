a8563bf95cf778c6e041075ea6b5bc9d53c8c3f6
Daniel Mathisen Breivik <danbre14@nfk.no>
2022-10-03 12:02:28 +0000

    added put request

--- pages/api/task/[id].ts
+++ pages/api/task/[id].ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import nextConnect from 'next-connect';
 import { v4 as uuidv4 } from 'uuid';
 import { Prisma, PrismaClient } from '@prisma/client';
-import { Task } from '.';
+import { guard, keyHandler, Task } from '.';
 interface errorResponse {
   error: string;
   errorRef: string;
@@ -15,7 +15,6 @@ interface TaskIDDeleteRequest extends NextApiRequest {
     id: string;
   };
 }
-
 interface TaskIDDeleteResponse {
   task: Prisma.tasksGetPayload<{}>;
 }
@@ -41,7 +40,6 @@ const handler = nextConnect({
   },
 })
   // GET /api/task/[id]
-  // TODO: #14 Get /api/task/[id] skal returnere en spesifik "task" fra databasen.\
   .get(async (req: NextApiRequest, res: NextApiResponse) => {
     // implemented
     try {
@@ -69,6 +67,56 @@ const handler = nextConnect({
       };
     }
   })
+  .put(async (req: TaskIDPutRequest, res: NextApiResponse) => {
+    try {
+      const prisma = new PrismaClient();
+      let { errors, allowed } = guard(req.body.task, new keyHandler().put());
+      if (!allowed) {
+        throw {
+          userMessage: 'Missing, wrong or invalid data',
+          debugMessage: errors.join(', '),
+          errorResponseCode: 400,
+        };
+      }
+      await prisma.$connect();
+      // Check if task exists
+      let task = await prisma.tasks.findUnique({
+        where: {
+          id: Number(req.query.id),
+        },
+      });
+      if (!task) {
+        throw {
+          userMessage: 'Task not found',
+          debugMessage: 'Task not found',
+          errorResponseCode: 404,
+        };
+      }
+      let updated = await prisma.tasks.update({
+        where: {
+          id: Number(req.query.id),
+        },
+        data: {
+          what: req.body.task.what,
+          where: req.body.task.where,
+          priority: req.body.task.priority,
+          start: req.body.task.start,
+          doneby: req.body.task.doneby,
+          done: req.body.task.done,
+        },
+      });
+      if (updated) {
+        return res.status(200).json({ task: updated });
+      }
+    } catch (error) {
+      throw {
+        userMessage: 'Error updating task',
+        debugMessage: error,
+        errorResponseCode: 500,
+      };
+    }
+  })
+
   .delete(
     async (
       req: TaskIDDeleteRequest,
