579e5d078ddad3a53a4944f4eec2d70612ce8426
Daniel Mathisen Breivik <danbre14@nfk.no>
2022-10-03 11:14:11 +0000

    Added get request

--- pages/api/task/[id].ts
+++ pages/api/task/[id].ts
@@ -30,8 +30,9 @@ interface TaskIDPutRequest extends NextApiRequest {
 // TODO: #12 Add middleware to handle errors and 405
 const handler = nextConnect({
   onError(error, req: NextApiRequest, res: NextApiResponse<errorResponse>) {
+    console.error(error);
     res.status(501).json({
-      error: `Sorry something Happened! ${error.message}`,
+      error: `Sorry something Happened! ${error.userMessage}`,
       errorRef: uuidv4(),
     });
   },
@@ -42,21 +43,44 @@ const handler = nextConnect({
   // GET /api/task/[id]
   // TODO: #14 Get /api/task/[id] skal returnere en spesifik "task" fra databasen.\
   .get(async (req: NextApiRequest, res: NextApiResponse) => {
-    // Not implemented
-    res.status(501).json({ error: 'Not implemented' });
+    // implemented
+    try {
+      const prisma = new PrismaClient();
+      await prisma.$connect();
+      let task = await prisma.tasks.findUnique({
+        where: {
+          id: Number(req.query.id),
+        },
+      });
+      await prisma.$disconnect();
+      if (!task) {
+        throw {
+          userMessage: 'Task not found',
+          debugMessage: 'Task not found',
+          errorResponseCode: 404,
+        };
+      }
+      res.status(200).json({ task: task });
+    } catch (error) {
+      throw {
+        userMessage: 'Error getting task',
+        debugMessage: error,
+        errorResponseCode: 500,
+      };
+    }
   })
   .delete(
     async (
       req: TaskIDDeleteRequest,
       res: NextApiResponse<TaskIDDeleteResponse>,
     ) => {
-      const prisma = new PrismaClient();
-      await prisma.$connect();
-      const id = req.query.id;
       try {
+        const prisma = new PrismaClient();
+        await prisma.$connect();
+        const id = req.query.id;
         const task = await prisma.tasks.delete({
           where: {
-            id: id as unknown as number,
+            id: Number(id),
           },
         });
         res.status(200).json({ task: task });
